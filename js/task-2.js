70c0e213c1ab0abf988aa1a922a9dee9415e024c
Vladyslav Svintsitskyi <svintsitskyi1999@gmail.com>
2024-08-28 20:32:16 +0200

    add js tasks

--- js/task-2.js
+++ js/task-2.js
@@ -1,9 +1,16 @@
-function getShippingMessage(country, price, deliveryFee) {
-  const totalPrice = price + deliveryFee;
-  const message = `Shipping to ${country} will cost ${totalPrice} credits`;
-  return message;
+function formatMessage(message, maxLength) {
+  const length = message.length;
+  if (length <= maxLength) {
+    return message;
+  } else {
+    const newMsg = message.slice(0, maxLength);
+    return `${newMsg}...`;
+  }
 }
 
-console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
-console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
-console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
+console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
+console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
+console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
+console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
